1e7e5ba7fc5eb8b4a3d673719e6339e40243a266
rohanpaudel22 <poudelrohan2060@gmail.com>
2025-02-26 22:42:10 +0545

    Adding Django Object-Relational Mapper(ORM) features

--- core/vege/admin.py
+++ core/vege/admin.py
@@ -5,3 +5,7 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Receipe)
+
+admin.site.register(StudentID)
+admin.site.register(Student)
+admin.site.register(Department)
---
+++ core/vege/seed.py
@@ -0,0 +1,35 @@
+from faker import Faker
+fake = Faker()
+import random
+from .models import *
+
+
+def seed_db(n = 10)->None:
+  
+  try:
+      for i in range(0 , n):
+        # [1 ,2 , 3 ,4]
+        departments_objs = Department.objects.all()
+        random_index = random.randint(0 , len(departments_objs)-1)
+        department = departments_objs[random_index]
+        student_id = f'STU-0{random.randint(100 , 999)}'
+        student_name = fake.name()
+        student_email = fake.email()
+        student_age = random.randint(20 , 30)
+        student_address = fake.address()
+        
+        
+        student_id_obj = StudentID.objects.create(student_id = student_id)
+        
+        
+        student_obj = Student.objects.create(
+          
+          department = department,
+          student_id = student_id_obj,
+          student_name = student_name,
+          student_email = student_email,
+          student_age = student_age,
+          student_address = student_address,
+        )
+  except Exception as e:
+    print(e)
