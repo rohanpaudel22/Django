242784d23f09e58b65128ddbcea1f5b24b72c010
rohanpaudel22 <poudelrohan2060@gmail.com>
2025-03-01 14:34:11 +0545

    Custom user model added

--- core/vege/models.py
+++ core/vege/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 class Receipe(models.Model):
   user = models.ForeignKey(User , on_delete=models.SET_NULL , null = True , blank= True)
   receipe_name = models.CharField(max_length=100)
--- core/vege/views.py
+++ core/vege/views.py
@@ -7,6 +7,10 @@ from django.contrib.auth import authenticate , login , logout
 from django.contrib.auth.decorators import login_required
 
 
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
 
 # Create your views here.
 
